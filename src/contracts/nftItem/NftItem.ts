cce802af82f91eadab733a46f9c89a9b5c1e37c4
Tim <truecarry@gmail.com>
2024-11-02 19:06:30 +0400

    Add single nft state data

--- src/contracts/nftItem/NftItem.ts
+++ src/contracts/nftItem/NftItem.ts
@@ -1,4 +1,5 @@
-import { Address, Cell, beginCell } from 'ton-core'
+import { Address, Cell, StateInit, beginCell, contractAddress, storeStateInit } from 'ton-core'
+import { encodeOffChainContent } from '../nft-content/nftContent'
 
 export const OperationCodes = {
   transfer: 0x5fcc3d14,
@@ -33,4 +34,61 @@ export function BuildTransferNftBody(params: {
   }
 
   return msgBody.endCell()
+}
+
+export const NftSingleCodeBoc =
+  'te6cckECFQEAAwoAART/APSkE/S88sgLAQIBYgcCAgEgBAMAI7x+f4ARgYuGRlgOS/uAFoICHAIBWAYFABG0Dp4AQgRr4HAAHbXa/gBNhjoaYfph/0gGEAICzgsIAgEgCgkAGzIUATPFljPFszMye1UgABU7UTQ+kD6QNTUMIAIBIA0MABE+kQwcLry4U2AEuQyIccAkl8D4NDTAwFxsJJfA+D6QPpAMfoAMXHXIfoAMfoAMPACBtMf0z+CEF/MPRRSMLqOhzIQRxA2QBXgghAvyyaiUjC64wKCEGk9OVBSMLrjAoIQHARBKlIwuoBMSEQ4BXI6HMhBHEDZAFeAxMjQ1NYIQGgudURK6n1ETxwXy4ZoB1NQwECPwA+BfBIQP8vAPAfZRNscF8uGR+kAh8AH6QNIAMfoAggr68IAboSGUUxWgod4i1wsBwwAgkgahkTbiIML/8uGSIY4+ghBRGkRjyFAKzxZQC88WcSRKFFRGsHCAEMjLBVAHzxZQBfoCFctqEssfyz8ibrOUWM8XAZEy4gHJAfsAEFeUECo4W+IQAIICjjUm8AGCENUydtsQN0UAbXFwgBDIywVQB88WUAX6AhXLahLLH8s/Im6zlFjPFwGRMuIByQH7AJMwMzTiVQLwAwBUFl8GMwHQEoIQqMsArXCAEMjLBVAFzxYk+gIUy2oTyx/LPwHPFsmAQPsAAIYWXwZsInDIywHJcIIQi3cXNSHIy/8D0BPPFhOAQHCAEMjLBVAHzxZQBfoCFctqEssfyz8ibrOUWM8XAZEy4gHJAfsAAfZRN8cF8uGR+kAh8AH6QNIAMfoAggr68IAboSGUUxWgod4i1wsBwwAgkgahkTbiIMIA8uGSIY4+ghAFE42RyFALzxZQC88WcSRLFFRGwHCAEMjLBVAHzxZQBfoCFctqEssfyz8ibrOUWM8XAZEy4gHJAfsAEGeUECo5W+IUAIICjjUm8AGCENUydtsQN0YAbXFwgBDIywVQB88WUAX6AhXLahLLH8s/Im6zlFjPFwGRMuIByQH7AJMwNDTiVQLwA+GNLv4='
+
+export const NftSingleCodeCell = Cell.fromBoc(Buffer.from(NftSingleCodeBoc, 'base64'))[0]
+
+export type RoyaltyParams = {
+  // numerator
+  royaltyFactor: number
+  // denominator
+  royaltyBase: number
+  royaltyAddress: Address
+}
+
+export type NftSingleData = {
+  ownerAddress: Address
+  editorAddress: Address
+  content: string
+  royaltyParams: RoyaltyParams
+}
+
+export function buildSingleNftDataCell(data: NftSingleData) {
+  const dataCell = beginCell()
+
+  const contentCell = encodeOffChainContent(data.content)
+
+  const royaltyCell = beginCell()
+  royaltyCell.storeUint(data.royaltyParams.royaltyFactor, 16)
+  royaltyCell.storeUint(data.royaltyParams.royaltyBase, 16)
+  royaltyCell.storeAddress(data.royaltyParams.royaltyAddress)
+
+  dataCell.storeAddress(data.ownerAddress)
+  dataCell.storeAddress(data.editorAddress)
+  dataCell.storeRef(contentCell)
+  dataCell.storeRef(royaltyCell)
+
+  return dataCell.endCell()
+}
+
+export function buildSingleNftStateInit(conf: NftSingleData) {
+  const dataCell = buildSingleNftDataCell(conf)
+
+  const stateInit: StateInit = {
+    code: NftSingleCodeCell,
+    data: dataCell,
+  }
+
+  const stateInitCell = beginCell().store(storeStateInit(stateInit)).endCell()
+
+  const address = contractAddress(0, stateInit)
+
+  return {
+    stateInit: stateInitCell,
+    stateInitMessage: stateInit,
+    address,
+  }
 }
